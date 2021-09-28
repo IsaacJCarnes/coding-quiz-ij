ed16f855d3e323db4207e68e88a8a7e4d69e6ed7
Isaac Joshua <isaacjcarnes@gmail.com>
2021-09-27 22:24:00 -0400

    start game button now changes the title to a question and adds answers (nothing can be chosen yet).

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,29 +1,53 @@
+const heading = document.getElementById("titleQuestion");
+const headingText = heading.innerHTML;
+
+const description = document.getElementById("description")
 const timer = document.getElementById("timer");
+const answerArea = document.getElementById("answer");
 var startBtn = document.createElement("BUTTON");
-startBtn.id = "startBtn";
-startBtn.innerHTML = "Start Game";
-document.body.appendChild(startBtn);
+
+const q1 = {question:"How do we search for an ID in Javascript?", answers: [".searchElementByID()", ".searchID()", ".getElementByID()", ".getID()"], isCorrect: [false, false, true, false]};
+/*const q2 = {question:"", answers: ["", "", ""]};
+const q3 = {question:"", answers: ["", "", ""]};
+const q4 = {question:"", answers: ["", "", ""]};
+const q5 = {question:"", answers: ["", "", ""]};*/
 
 //var imgTag = document.createElement("img");
 
 //imgTag.setAttribute("src", "Assets/images/hoopla-is-dead.png");
 //imgTag.setAttribute("alt", "Start Test");
 
-/*imgTag.addEventListener("click", function(){
-    gameTimer();
-});*/
 
 startBtn.addEventListener("click", function(){
-    gameTimer();
-    startBtn.style.display = "none";
-});
+  startBtn.style.display = "none";
+  description.style.display = "none";
+  answerArea.style.visibility = "visible";
+  addAnswers(q1);
+  gameTimer();
+});  
+
+function addAnswers(prompt){
+  heading.innerHTML = prompt.question;
+  for(i = 0; i < prompt.answers.length; i++){
+    var li = document.createElement('li');
+    var text = document.createTextNode(prompt.answers[i]);
+
+    if(prompt.isCorrect[i]){
+      li.dataset.isCorrect = true;
+    } else {
+      li.dataset.isCorrect = false;
+    }
+
+    li.appendChild(text);
+    answerArea.appendChild(li);
+  }
+}
 
 function gameTimer() {
     var timeLimit = 75;
   
     var timeInterval = setInterval(function () {
       timeLimit--;
-      console.log(timeLimit);
       
       timer.innerHTML = `${timeLimit} seconds`;
       if(timeLimit <= 0){
@@ -31,3 +55,7 @@ function gameTimer() {
       }
     }, 1000);
 }
+
+startBtn.id = "startBtn";
+startBtn.innerHTML = "Start Game";
+document.body.appendChild(startBtn);
